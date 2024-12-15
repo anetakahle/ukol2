e5f521205badd8f3474d18af819a61fcdea6bca7
Aneta Kahleová <aneta.kahleova@gmail.com>
2024-12-15 22:09:02 +0100

    initial commit

---
+++ ukol2/ukol/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length != 3)
+        {
+            Console.WriteLine("Chyba: Musíš zadat přesně 3 čísla jako vstup.");
+            return;
+        }
+
+        double cislo1, cislo2, cislo3;
+
+        try
+        {
+            cislo1 = double.Parse(args[0]);
+            cislo2 = double.Parse(args[1]);
+            cislo3 = double.Parse(args[2]);
+        }
+        catch
+        {
+            Console.WriteLine("Chyba: Všechny vstupy musí být čísla.");
+            return;
+        }
+
+        try
+        {
+            double soucet = cislo1+cislo2+cislo3;
+            double soucin = cislo1*cislo2*cislo3;
+
+            // třetí číslo je nula
+            if (cislo3 == 0)
+            {
+                Console.WriteLine("Chyba: Nelze dělit nulou.");
+                return;
+            }
+
+            double podil = (cislo1+cislo2)/cislo3;
+
+            // výstup výsledků
+            Console.WriteLine($"součet: {soucet}");
+            Console.WriteLine($"součin: {soucin}");
+
+            // podíl je příliš velký, nebo malý
+            if (double.IsInfinity(podil))
+            {
+                Console.WriteLine("podíl je příliš velký na zobrazení.");
+            }
+            else
+            {
+                Console.WriteLine($"podíl: {podil}");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"chyba: {ex.Message}");             // obecná chyba
+        }
+    }
+}
